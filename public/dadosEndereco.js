6ae9e361bf9e3103d46ce30dfa4fe32fd1adde5b
JoaoVitordSRodrigues <joaovitor10sr@gmail.com>
2024-08-31 16:14:32 -0300

    finalizando-front/implement-func-focus-viacep

--- public/dadosEndereco.js
+++ public/dadosEndereco.js
@@ -1,10 +1,12 @@
 import ViaCepService from './ViaCepService.js';
 
+//carregando a lista dos estados existentes
 async function carregarEstados() {
     try {
         const response = await fetch('https://servicodados.ibge.gov.br/api/v1/localidades/estados');
         const estados = await response.json();
 
+        //inserindo no campo todos os estados
         const estadoSelect = document.getElementById('uf');
         estados.forEach(estado => {
             const option = document.createElement('option');
@@ -20,12 +22,21 @@ async function carregarEstados() {
 // Chama a função para carregar os estados assim que a página é carregada
 carregarEstados();
 
+
+// quando o campo 'cep' perder o foco, preenche o campo de acordo com o cep inserido
+cep.addEventListener("blur", async (event) => {
+    await buscaCep();
+}, true);
+
+
+//busca dados do endereco
 async function buscaCep() {
-    const cep = document.getElementById('cep').value;
+    const cep = document.getElementById('cep').value;//valor do cep inserido
     const viaCepService = new ViaCepService();
 
     try {
         const result = await viaCepService.operation(cep);
+        //preenchendo o campo com o dados retornado do arquivo viacepservice
         document.getElementById('uf').value = result.data.uf || '';
         document.getElementById('bairro').value = result.data.bairro || '';
         document.getElementById('cidade').value = result.data.localidade || '';
@@ -36,6 +47,3 @@ async function buscaCep() {
     }
 }
 
-cep.addEventListener("blur", async (event) => {
-    await buscaCep();
-}, true);
