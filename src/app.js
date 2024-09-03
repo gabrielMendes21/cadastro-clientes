21b66701964baaace1b4032ba51ac7e4bf5ef699
Gabriel Mendes <gabriel.26056@gmail.com>
2024-09-03 12:50:44 -0300

    add 'cache cep' functionality

--- src/app.js
+++ src/app.js
@@ -7,10 +7,17 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const Cliente = require('./models/Cliente.js');
 
+// Cache para os CEPs
+
+// Cache simples
+const cache = {};
+
+// Cache com node-cache
+// ...
+
 // Conexão com banco local
 const conn = require('./db/conn.js');
 conn();
-
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -36,8 +43,35 @@ app.post("/cadastrar", async (req, res) => {
 })
 
 // Armazenar as informações do CEP em cache
-app.post("/cacheCEP", (req, res) => {
-    res.json("Cache dos CEPs");
+app.post("/cacheCEP", async (req, res) => {
+    try {
+        const { CEP } = req.body;
+
+        const cachedCEP = cache[CEP];
+
+        // Verifica se o CEP fornecido já está em cache
+        // Caso ele não esteja, uma nova requisição (cujos dados da resposta serão armazenados em cache) 
+        // é feita para a API o ViaCEP para a obtenção do novo CEP
+        if (cachedCEP) {
+            console.log(cache);
+            res.json(cachedCEP);
+        } else {
+            const response = await fetch(`https://viacep.com.br/ws/${CEP}/json/`);
+            const CEPData = await response.json();
+
+            // Armazenamento dos dados do CEP em cache
+            cache[CEP] = CEPData;
+
+            console.log(cache);
+
+            res.json(CEPData);
+        }
+    } catch(err) {
+        console.log({
+            msg: "Erro ao buscar dados do CEP",
+            err
+        })
+    }
 })
 
 // Verificar se o CEP fornecido está em cache
