e96b6210b36c507bb16bcc942c8ed6017f7d459c
Gabriel Mendes <gabriel.26056@gmail.com>
2024-09-06 15:24:47 -0300

    gabriel: add remote connection to mongo database

--- src/db/mongoDB.js
+++ src/db/mongoDB.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 const conn = async () => {
     try {
-        await mongoose.connect(`mongodb://localhost:27017/${process.env.DBNAME}`);
+        // await mongoose.connect(`mongodb://localhost:27017/${process.env.DBNAME}`);
+        await mongoose.connect(process.env.MONGO_URI);
 
         console.log("Conectado ao banco mongodb");
     } catch(err) {
